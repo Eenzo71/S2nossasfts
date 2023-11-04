4aa966203cefa8d6e81a88c1da2fea18d43e3d3c
Eenzo71 <149822547+Eenzo71@users.noreply.github.com>
2023-11-03 21:59:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const loginButton = document.getElementById("login-button");
 
   loginButton.addEventListener("click", function (event) {
-    event.preventDefault(); // Evita que o formul·rio seja enviado
+    event.preventDefault(); // Evita que o formul√°rio seja enviado
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
     if (username === "q" && password === "1") {
       alert("Login bem-sucedido!");
-      window.location.href = "https://codepen.io/enzo-emanuel/pen/dyaPZyj";
+      window.location.href = "";
     } else {
-      alert("Login falhou. Verifique o nome de Usu·rio e Senha.");
+      alert("Login falhou. Verifique o nome de Usu√°rio e Senha.");
     }
   });
 });
